0f2e8bf2f588d59ead801ef697226d2606e08cc7
hwe-bremen <plan2-hw@dobben-united.de>
2020-12-19 21:22:08 +0100

    19-dez-03

---
+++ site/templates/basic-page.php
@@ -0,0 +1,27 @@
+<body>
+
+            <?php snippet('menu') ?>
+            <?php snippet('header_unterseiten') ?>
+
+
+<section id="basic-page" >
+
+  <article class="grid-blog">
+  <p class="datum"><?= $page->published()->toDate('d.m.Y') ?></p>
+      <h2 class="basic-page_head"><?= $page->headline()->html() ?></h2>
+        <?= $page->image() ?>
+        <h3><?= $page->introtext()->html() ?> </h3>
+     
+       <?= $page->textarea()->kirbytext() ?>
+      <a href="<?= url('blog') ?>">Back…</a>
+    </article> 
+</section> 
+
+
+</main>
+
+<footer class="footer"> Hier steht noch nichts</footer>
+
+
+</body>
+</html>
